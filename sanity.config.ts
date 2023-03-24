71818e43109734e4c9b975b2f19a8df3b42b0835
4FRO <drotich@ucsc.edu>
2023-03-23 17:35:03 -0700

    Integrated Iframe in Studio Preview

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 import { schemaTypes } from './schemas';
+import { getDefaultDocumentNode } from './structure';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
@@ -14,7 +15,9 @@ export default defineConfig({
   projectId,
   dataset,
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool({
+    defaultDocumentNode: getDefaultDocumentNode
+  }), visionTool()],
 
   schema: {
     types: schemaTypes,
---
+++ structure.ts
@@ -0,0 +1,33 @@
+import Iframe from 'sanity-plugin-iframe-pane';
+import type { DefaultDocumentNodeResolver } from 'sanity/desk';
+
+export const getDefaultDocumentNode: DefaultDocumentNodeResolver = (
+  S,
+  { schemaType }
+) => {
+  if (schemaType === 'post') {
+    return S.document().views([
+      S.view.form(),
+
+      S.view
+        .component(Iframe)
+        .options({
+          // Required: Accepts an async function
+          // OR a string
+          url: `${
+            process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000'
+          }/api/preview`,
+          // Optional: Set the default size
+          defaultSize: `desktop`, // default `desktop`
+          // Optional: Add a reload button, or reload on new document revisions
+          reload: {
+            button: true,
+          },
+          // Optional: Pass attributes to the underlying `iframe` element:
+          // See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe
+          attributes: {},
+        })
+        .title('Preview'),
+    ]);
+  }
+};
